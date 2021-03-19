a923983da3f78406a5cf1c7236ada065fe142924
Md. Abdullah <abdullahkafi996@gmail.com>
2021-03-19 15:04:48 +0600

    add google authentication methdo

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,45 @@
 import React, { useContext } from 'react';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+
+
 
 
 const Login = () => {
+    firebase.initializeApp(firebaseConfig);
+
+    const handleGoogleSignIn = () => {
+        var googleProvider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth()
+            .signInWithPopup(googleProvider)
+            .then((result) => {
+                /** @type {firebase.auth.OAuthCredential} */
+                var credential = result.credential;
+
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                var token = credential.accessToken;
+                // The signed-in user info.
+                var user = result.user;
+                // ...
+            }).catch((error) => {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // The email of the user's account used.
+                var email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                var credential = error.credential;
+                // ...
+            });
+
+
+    }
+
     return (
         <div>
             <h1>This is Login</h1>
+            <button onClick={handleGoogleSignIn}>Google Sign In</button>
         </div>
     );
 };
