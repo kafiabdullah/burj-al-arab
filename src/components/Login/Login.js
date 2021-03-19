54ce0a307298cb707bd450fcdf448330528afb1d
Md. Abdullah <abdullahkafi996@gmail.com>
2021-03-19 15:47:28 +0600

    add private route and protect a path

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,42 +2,47 @@ import React, { useContext } from 'react';
 import firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from './firebase.config';
+import { UserContext } from '../../App';
+import { useHistory, useLocation } from 'react-router';
 
 
 
 
 const Login = () => {
-    firebase.initializeApp(firebaseConfig);
+    if (firebase.apps.length === 0) {
+        firebase.initializeApp(firebaseConfig);
+
+    }
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext)
+    const history = useHistory();
+    const location = useLocation();
+    const { from } = location.state || { from: { pathname: "/" } };
+
 
     const handleGoogleSignIn = () => {
         var googleProvider = new firebase.auth.GoogleAuthProvider();
         firebase.auth()
             .signInWithPopup(googleProvider)
             .then((result) => {
-                /** @type {firebase.auth.OAuthCredential} */
-                var credential = result.credential;
 
-                // This gives you a Google Access Token. You can use it to access the Google API.
-                var token = credential.accessToken;
-                // The signed-in user info.
-                var user = result.user;
-                // ...
+                const { displayName, email } = result.user;
+                const signedInUser = { name: displayName, email };
+                setLoggedInUser(signedInUser);
+                history.replace(from)
+
             }).catch((error) => {
-                // Handle Errors here.
                 var errorCode = error.code;
                 var errorMessage = error.message;
-                // The email of the user's account used.
                 var email = error.email;
-                // The firebase.auth.AuthCredential type that was used.
                 var credential = error.credential;
-                // ...
+                console.log(errorCode, errorMessage, email, credential);
             });
 
 
     }
 
     return (
-        <div>
+        <div style={{textAlign:'center'}}> 
             <h1>This is Login</h1>
             <button onClick={handleGoogleSignIn}>Google Sign In</button>
         </div>
