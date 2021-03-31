fc869e8b133298305d9ce53692deebaa8e048b7b
kafiabdullah <abdullahkafi996@gmail.com>
2021-03-31 15:35:12 +0600

    show loggedin user name on book page

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAgTjk5oxlG7sHNXAsPxvdnQuaQ9zuv2VA",
+    authDomain: "burj-al-arab-7023e.firebaseapp.com",
+    projectId: "burj-al-arab-7023e",
+    storageBucket: "burj-al-arab-7023e.appspot.com",
+    messagingSenderId: "386228765461",
+    appId: "1:386228765461:web:40527bfc2ab1d560b61f1c"
+};
+  
+export default firebaseConfig;
